a21eb01e05665a5842343d9e53141bafc1c761c5
Antoine Stjepanovic <apbstep@gmail.com>
2020-10-19 21:07:38 +0200

    first commit

---
+++ lib/gossip.rb
@@ -0,0 +1,31 @@
+
+class Gossip
+
+	attr_accessor :author, :content
+
+	def initialize(gossip_author, gossip_content)
+		@author = gossip_author
+		@content = gossip_content
+	end
+
+	def save
+		CSV.open("./db/gossip.csv", "ab") do |csv|
+			csv << [@author, @content]
+		end
+	end
+
+	def self.all
+	all_gossips = []
+	CSV.read("./db/gossip.csv").each do |csv_line|
+		all_gossips << Gossip.new(csv_line[0], csv_line[1])
+	end
+	return all_gossips
+
+	def self.find(id)
+		index_gossip = Gossip.all
+		return Gossip(id.index)
+
+	end
+end
+
+end
